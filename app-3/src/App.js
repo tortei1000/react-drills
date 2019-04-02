bb11aad7d487e54d68d840bc1d833cd1bd317b13
tiago maffei <tiagomaffei@gmail.com>
2019-04-02 14:18:15 -0600

    third

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      userInput: "",
+      array: ["banana", "agua", "mundo", "english"]
+    }
+  }
+  
+  onChange (val) {
+    this.setState({userInput: val})
+  }
+  
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+    let displayArray = this.state.array
+      .filter((element, index) => {
+        return element.includes(this.state.userInput)
+        
+      })
+      .map((element, index)=>{
+        return <h2 key={index}>{element}</h2>
+      })
+    
+    
+    
+    return ( 
+      <div className="App">   
+      <input className="inputArea" onChange={e=>this.onChange(e.target.value)} type="text"/>
+      {displayArray}  
+      </div>)
+    
   }
 }
 
