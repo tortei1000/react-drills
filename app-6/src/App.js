180bd2142cf7069645a1f55240d01494d8ce8a81
tiago maffei <tiagomaffei@gmail.com>
2019-04-02 15:36:28 -0600

    sixth

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import ToDo from "./todo"
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      array : [],
+      userInput : ""
+    }
+  }
+
+  handleChange =(val)=> {
+    this.setState({userInput: val})
+  }
+
+  addItem = (userInput)=> {
+    let arr = [...this.state.array]
+    arr.push(userInput)
+    this.setState({array:arr})
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+       <input onChange={e => this.handleChange(e.target.value)} />
+       <button onClick ={ () => { this.addItem(this.state.userInput) }}> Add Item </button>
+       <ToDo task ={this.state.array}/>
       </div>
     );
   }
---
+++ app-6/src/todo.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+function ToDo(props){
+  return<p>{props.task.map(e => <li>{e}</li>)}</p>
+}
+
+export default ToDo
